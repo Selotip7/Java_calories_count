8459521497a8a882bd645d1ee416388d56b9f4be
Selotip7 <selotip7@gmail.com>
2025-03-12 22:29:14 +0700

    tambah output

--- java.js
+++ java.js
@@ -2,15 +2,18 @@ const budgetInput = document.getElementById("budgetCalories");
 //untuk mengambil value dari select
 const entryDropdown = document.getElementById("entry-dropdown");
 const addEntryButton = document.getElementById("add-entry-btn");
-let isError=false;
+const output=document.getElementById("output");
+const submit = document.getElementById("sub");
+
+let isError = false;
 function isValid(str) {
   const regex = /\d+e\d+/i;
   return str.match(regex);
 }
 
-function clearInput(str){
-  const regex=/[+-\s]/;
-  return str.replace(regex, '');
+function clearInput(str) {
+  const regex = /[+-\s]/;
+  return str.replace(regex, "");
 }
 
 function addEntry() {
@@ -20,7 +23,7 @@ function addEntry() {
     `${targetId} #input-container`
   );
   const entryNumber =
-    targetInputContainer.querySelectorAll('input[type="text"]').length+1;
+    targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
 
   const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
@@ -32,36 +35,55 @@ function addEntry() {
     id="${entryDropdown.value}-${entryNumber}-calories"
     placeholder="Calories"
   />`;
-  targetInputContainer.insertAdjacentHTML("beforeend",HTMLString);
-//   targetInputContainer.innerHTML="makan";
+  targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+  //   targetInputContainer.innerHTML="makan";
 }
 
-function getCalorieFromInput(list){
-  let calorie=0;
-  for(const item of list ){
-    
-    const clearString=clearInput(item.value);
-    const validOra=isValid(clearString);
-    if(clearString){
+function getCalorieFromInput(list) {
+  let calorie = 0;
+  for (const item of list) {
+    const clearString = clearInput(item.value);
+    const validOra = isValid(clearString);
+    if (validOra) {
       alert(`Invalid input:${clearString[0]}`);
-      isError=true;
+      isError = true;
       return null;
-    } 
+    }
 
-    calorie+=Number(clearString);
-
+    calorie += Number(clearString);
   }
   return calorie;
 }
 
-function calculateCalories(e){
+function calculateCalories(e) {
   e.preventDefault();
-  isError=false;
-
+  isError = false;
   
-
-
+  // const breakfastCalories = document.querySelectorAll("#Breakfast input[type='number']");
+  
+  const breakfastCaloriesFinal = getCalorieFromInput(
+    document.querySelectorAll("#Breakfast input[type='number']")
+  );
+  // const dinnerCalories = getCaloriesFromInputs(
+    //   document.querySelectorAll('#Dinner input[type="number"]')
+    // );
+    // const lunchCalories = getCaloriesFromInputs(
+      //   document.querySelectorAll('#Lunch input[type="number"]')
+      // );
+      // const snacksCalories = getCalorieFromInput(
+        //   document.querySelectorAll('#Snacks input[type="number"]')
+        // );
+        // const exerciseCalories = getCalorieFromInput(
+          //   document.querySelectorAll('#Exercise input[type="number"]')
+          // );
+          
+         
+  const outHTML = ` <p>${breakfastCaloriesFinal} Calories Budgeted</p>`;
+  output.innerHTML = outHTML;
+  output.classList.remove("hide");
 }
 
 addEntryButton.onclick = addEntry;
+submit.addEventListener("click",calculateCalories);
+
 
